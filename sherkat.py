71b42f541fae0ef3ac2e10ccb630af4059aeaa00
shirin-ux <64709896+shirin-ux@users.noreply.github.com>
2020-07-14 21:21:15 +0430

    Create sherkat.py

---
+++ sherkat.py
@@ -0,0 +1,8 @@
+x=float(input("Enter the auto price:"))
+y=float(input("Enter the price of the paper:"))
+a=150
+b=50
+r=((float(input("Enter The inflation rate:")))/100)
+z=(a*x+y*b)
+f=(z*r)+z
+print("The inflation rate:",f)
